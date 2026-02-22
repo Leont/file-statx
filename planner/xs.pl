159cd335dec28f17a0f8635534d5081888054ede
Leon Timmermans <fawaka@gmail.com>
2026-02-22 01:47:28 +0100

    Use auto_PL in planner

--- planner/xs.pl
+++ planner/xs.pl
@@ -1,12 +1,5 @@
+load_extension('Dist::Build::Core');
+auto_PL();
+
 load_extension('Dist::Build::XS');
-
-create_node(
-	target => 'lib/File/StatX.xs',
-	dependencies => [ 'lib/File/StatX.xs.PL' ],
-	actions => [
-		command(perl_path(), 'lib/File/StatX.xs.PL', 'lib/File/StatX.xs'),
-
-	]
-);
-
 auto_xs();
